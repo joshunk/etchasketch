35cd520564bedd2d82ced1d83064ee027db8f4bf
Joshua Shunk <aerixia@gmail.com>
2023-07-31 18:08:34 -0400

    Add in loop to create numbered divs with numbers and colors

---
+++ main.js
@@ -0,0 +1,9 @@
+let divGrid = document.querySelector('#container');
+
+for (let i = 1; i <= 16; i++) {
+	let newDiv = document.createElement('div');
+    newDiv.textContent = (i);
+	
+	container.appendChild(newDiv);
+}
+
